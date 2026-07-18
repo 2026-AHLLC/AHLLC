0b8afe952f5809c2f23b2e0fbbff7137cf906ab0
2026-AHLLC <johnegan2025@gmail.com>
2026-07-18 09:16:48 -0400

    Create textarea.tsx

---
+++ textarea.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  error?: boolean;
+}
+
+const
