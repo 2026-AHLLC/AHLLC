73c6c857c0165764a198318f4c87722b1ea4875d
2026-AHLLC <johnegan2025@gmail.com>
2026-07-18 09:39:09 -0400

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -179,6 +179,7 @@ export default function RootLayout({
         {children}
 
         <Analytics />
+        import { Toaster } from "@/components/ui/toaster";
 
         <SpeedInsights />
       </body>
