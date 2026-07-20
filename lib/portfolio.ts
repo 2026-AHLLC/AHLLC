4cad617ffdc8aa61c19df793c675498b3c8ee14e
2026-AHLLC <johnegan2025@gmail.com>
2026-07-20 13:19:16 -0400

    fixed bugs

--- lib/portfolio.ts
+++ lib/portfolio.ts
@@ -10,6 +10,15 @@ export interface PortfolioProject {
   results: string[];
 }
 
+import johnnysStudio from "@/content/portfolio/johnnys-studio";
+import bewiseContractors from "@/content/portfolio/bewise-contractors";
+import suffolkNyPoolServices from "@/content/portfolio/suffolk-ny-pool-services";
+
+export const portfolio = [
+  johnnysStudio,
+  bewiseContractors,
+  suffolkNyPoolServices,
+];
 
 export function getProject(slug: string) {
   return portfolio.find((p) => p.slug === slug);
