071ef80dab194b19be4808a4dc917b12e6cbffaa
2026-AHLLC <johnegan2025@gmail.com>
2026-07-27 00:28:31 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   typedRoutes: true,
+
+  experimental: {
+    serverActions: {
+      // Allows multipart Server Action requests with enough overhead
+      // for application-level file uploads up to 25 MB.
+      bodySizeLimit: "27mb",
+    },
+  },
 };
 
 export default nextConfig;
